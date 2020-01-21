77d2c7838a8f37fd803d02eab005945104d85065
赤道企鹅 <40513718+yikesoftware@users.noreply.github.com>
2020-01-21 15:04:58 +0800

    线程优化
    线程优化

--- Spider_of_t66y.py
+++ Spider_of_t66y.py
@@ -3,6 +3,8 @@ import threading
 from bs4 import BeautifulSoup
 from lxml import html
 import xml
+import sys
+import argparse 
 import os
 import time
 '''
@@ -12,11 +14,7 @@ class_list1=["[亞洲]","[歐美]","[動漫]","[寫真]","[原创]","[其它]"]
 main_url="http://t66y.com/"
 url1="http://t66y.com/thread0806.php?fid=8&search=&page="
 url2="http://t66y.com/thread0806.php?fid=16&search=&page="
-
-if os.path.exists("./t66y"):
-    print("path[',/t66y'] exists")
-else:
-    os.mkdir("./t66y")
+max_thread=200
 
 def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
     count=-1
@@ -38,10 +36,11 @@ def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
     photo_div=soup.find_all('div',class_="tpc_content do_not_catch")
     photo_list=photo_div[0].find_all('img')
     savepath=path+"/"+name[:4]+"/"+name[4:]
+    photo_num=len(photo_list)
     for li in photo_list:
-        print(str(li))
+        #print(str(li))
         pic_url=str(li).split('"')[-2]
-        print(pic_url)
+        print("download:",pic_url,end="")
         try:
             r=requests.get(pic_url,stream=True)
         except:
@@ -50,6 +49,7 @@ def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
         if r.status_code==200:
             count+=1
             open(savepath+"/"+str(count)+"."+pic_url.split(".")[-1], 'wb').write(r.content)
+            print("(",count+1,"/",photo_num,")")
         else:
             print("request failed!")
         del r
@@ -86,17 +86,68 @@ def get_list(class_name,url): #该函数获取板块内的帖子列表
     print(post_list)
     for key in post_list:
         #download_pic(key,post_list[key],"./t66y/"+class_name)
+        while(1):
+            if threading.active_count()<max_thread:
+                break
+            else:
+                time.sleep(1)
         download_thread=threading.Thread(target=download_pic, args=(key,post_list[key],"./t66y/"+class_name,)) #多线程下载
+        download_thread.setDaemon(True) 
         download_thread.start()
-        time.sleep(0.5)
+        time.sleep(0.1)
 
-print("适当看图怡情，过度手冲伤身！")
-page_to_down=int(input("输入你要下载的页数（大于1）："))
-if page_to_down<1:
-    exit()
-else:
-    if page_to_down==1:
-        page_to_down+=1
-for i in range(1,page_to_down):
-    get_list("新時代的我們",url1+str(i))
-    get_list("達蓋爾的旗幟",url2+str(i))
+def pre_exit():
+    while(1):
+        thread_unfinished=threading.active_count()
+        if thread_unfinished>0:
+            print("***剩余下载线程：[",thread_unfinished,"]***")
+            print("若长时间无反应请手动结束进程...")
+            time.sleep(8)
+        else:
+            print("下载已完成！")
+            exit(0)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--class_id',type=int,default=0,help="'1' for 【新時代的我們】, '2' for 【達蓋爾的旗幟】, '0' for both")
+    parser.add_argument('-s','--start',type=int,default=1,help="Page_start(default=1)")
+    parser.add_argument('-e','--end',type=int,default=1,help="Page_end")
+    parser.add_argument('-m','--max_thread',type=int,default=200,help="Max thread num(default=200)")
+    args = parser.parse_args()
+    class_id=args.class_id
+    start=args.start
+    end=args.end
+    if class_id>2:
+        print("Sorry no class [",class_id,"] !")
+        exit(0)
+    if end<start or start<1:
+        print("Bad range!")
+        exit(0)
+    if os.path.exists("./t66y"):
+        print("path[',/t66y'] exists")
+    else:
+        os.mkdir("./t66y")
+    print("Enjoy your life!")
+    if class_id==0:
+        print("将下载【新時代的我們】和【達蓋爾的旗幟】的图片...")
+        for i in range(start,end+1):
+            print("开始下载第",i,"页")
+            get_list("新時代的我們",url1+str(i))
+            get_list("達蓋爾的旗幟",url2+str(i))
+        pre_exit()
+    else:
+        if class_id==1:
+            print("将下载【新時代的我們】的图片...")
+            for i in range(start,end+1):
+                print("开始下载第",i,"页")
+                get_list("新時代的我們",url1+str(i))
+            pre_exit()
+        if class_id==2:
+            print("将下载【達蓋爾的旗幟】的图片...")
+            for i in range(start,end+1):
+                print("开始下载第",i,"页")
+                get_list("達蓋爾的旗幟",url2+str(i))
+            pre_exit() 
+
+if __name__=="__main__":
+    main()
