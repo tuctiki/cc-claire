15c3781b991178346b767f87c71c97534ce90132
赤道企鹅 <40513718+yikesoftware@users.noreply.github.com>
2020-01-31 20:56:35 +0800

    设置UA，设置timeout，添加取消代理的参数
    设置UA，设置timeout，添加取消代理的参数

--- Spider_of_t66y.py
+++ Spider_of_t66y.py
@@ -13,7 +13,11 @@ class_list1=["[亞洲]","[歐美]","[動漫]","[寫真]","[原创]","[其它]"]
 main_url="http://t66y.com/"
 url1="http://t66y.com/thread0806.php?fid=8&search=&page="
 url2="http://t66y.com/thread0806.php?fid=16&search=&page="
-max_thread=200
+max_thread=300
+useProxy=True
+#UA设置
+head = {}
+head['User-Agent'] = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML,  like Gecko) Chrome/18.0.1025.166  Safari/535.19'
 
 try:
     proxy=open("proxy","r")
@@ -22,6 +26,12 @@ try:
 except Exception as e:
     print("Filed to load './proxy' cause:",e)
     sys.exit(0)
+
+def myRequest_get(url,stream=False,timeout=(5,30)):
+    if useProxy:
+        return requests.get(url,proxies=proxies,headers=head,stream=stream,timeout=(5,30))
+    else:
+        return requests.get(url,headers=head,stream=stream,timeout=(5,30))
 
 def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
     count=-1
@@ -35,7 +45,7 @@ def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
     else:
         os.mkdir(path+"/"+name[:4]+"/"+name[4:])
     try:
-        f=requests.get(url,proxies=proxies)
+        f=myRequest_get(url,False)
     except Exception as e:
         print("download failed:",e)
         return 0
@@ -49,16 +59,20 @@ def download_pic(name,url,path): #该函数用于下载具体帖子内的图片
         pic_url=str(li).split('"')[-2]
         print("download:",pic_url,end="")
         try:
-            r=requests.get(pic_url,proxies=proxies,stream=True)
+            r=myRequest_get(url,True)
         except Exception as e:
             print("connect failed:",e)
             return 0
         if r.status_code==200:
             count+=1
-            open(savepath+"/"+str(count)+"."+pic_url.split(".")[-1], 'wb').write(r.content)
-            print("(",count+1,"/",photo_num,")")
+            try:
+                savefilename=savepath+"/"+str(count)+"."+pic_url.split(".")[-1]
+            except:
+                savefilename=savepath+"/"+str(count)+".jpg"
+            open(savefilename, 'wb').write(r.content)
+            print("(%d/%d)"%(count+1,photo_num))
         else:
-            print(r.status_code,":request failed!")
+            print(r.status_code,":url(%s) request failed!"%(pic_url))
         del r
 
 def get_list(class_name,url): #该函数获取板块内的帖子列表
@@ -69,7 +83,7 @@ def get_list(class_name,url): #该函数获取板块内的帖子列表
         os.mkdir("./t66y/"+class_name)
     post_class=""
     try:
-        f=requests.get(url,proxies=proxies)
+        f=myRequest_get(url,False)
     except Exception as e:
         print("Connect failed:",e)
         sys.exit(0)
@@ -119,11 +133,15 @@ def main():
     parser.add_argument('-c','--class_id',type=int,default=0,help="'1' for 【新時代的我們】, '2' for 【達蓋爾的旗幟】, '0' for both")
     parser.add_argument('-s','--start',type=int,default=1,help="Page_start(default=1)")
     parser.add_argument('-e','--end',type=int,default=1,help="Page_end")
-    parser.add_argument('-m','--max_thread',type=int,default=200,help="Max thread num(default=200)")
+    parser.add_argument('-m','--max_thread',type=int,default=300,help="Max thread num(default=300)")
+    parser.add_argument('-p','--noproxy',type=int,default=1,help="'0': not use proxy; '1': use proxy; (default=1)")
     args = parser.parse_args()
     class_id=args.class_id
     start=args.start
     end=args.end
+    if args.noproxy==0: #关闭代理
+        useProxy=False
+
     if class_id>2:
         print("Sorry no class [",class_id,"] !")
         sys.exit(0)
